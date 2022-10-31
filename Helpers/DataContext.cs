f84fb035b7cff5d2b1128c3d5594cd25db6df5db
Talahzaram <ziggystar892@gmail.com>
2022-10-30 23:45:06 -0400

    Changed logo and moved AWS connection

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -16,7 +16,7 @@ namespace Schedule.Helpers
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // connect to mysql with connection string from app settings
-            var connectionString = Configuration.GetConnectionString("LocalDatabase");
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
             options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
 
