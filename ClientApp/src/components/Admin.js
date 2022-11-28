720140dc533657ed9d191345cfe9162cc794c2f2
AcheronJay <jaydeon96@hotmail.com>
2022-11-27 21:50:51 -0500

    Admin Test

---
+++ ClientApp/src/components/Admin.js
@@ -0,0 +1,69 @@
+let courses = [
+    {
+      id: 1,
+      text: "testCourse 1",
+      start: "2023-01-15T09:30:00",
+      end: "2023-01-15T10:40:00",
+      barColor: "#fcb711",
+      resource: "TH",
+      instuctor:"Smith",
+      room:"Wall 234",
+      isOpen:"0"
+    },
+    {
+      id: 2,
+      text: "testCourse 2",
+      start: "2023-01-15T09:30:00",
+      end: "2023-01-15T10:00:00",
+      barColor: "#f37021",
+      resource: "M",
+      instuctor:"Gilmore",
+      room:"Wall 234",
+      isOpen:"1"
+    },
+    {
+      id: 3,
+      text: "testCourse 3",
+      start: "2023-01-15T10:00:00",
+      end: "2023-01-15T11:00:00",
+      barColor: "#cc004c",
+      resource: "M",
+      instuctor:"Smith",
+      room:"Wall 234",
+      isOpen:"1"
+    },
+    {
+      id: 4,
+      text: "testCourse 4",
+      start: "2023-01-15T11:00:00",
+      end: "2023-01-15T12:00:00",
+      barColor: "#6460aa",
+      resource: "W",
+      instuctor:"Gilmore",
+      room:"Wall 234",
+      isOpen:"1"
+    },
+    {
+      id: 5,
+      text: "testCourse 5",
+      start: "2023-01-15T13:00:00",
+      end: "2023-01-15T14:00:00",
+      resource: "F",
+      instuctor:"Jones",
+      room:"Wall 234",
+      isOpen:"1"
+    },
+    {
+      id: 6,
+      text: "testCourse 6",
+      start: "2023-01-15T12:30:00",
+      end: "2023-01-15T15:30:00",
+      barColor: "#f1c232",
+      resource: "TU",
+      instuctor:"Gilmore",
+      room:"Wall 234",
+      isOpen:"1"
+  
+    },
+  ];
+  
